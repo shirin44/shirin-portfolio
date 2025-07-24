b5c64f6a826f663b0e8c3b2c2b4b6f5f39057995
shirin44 <S3983427@rmit.edu.vn>
2025-07-24 15:41:44 +0700

    Build: deploy to docs folder

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -200,7 +200,7 @@ export default function Experience() {
                       onError={(e) => {
                         e.target.onerror = null;
                         e.target.src =
-                          "src/assets/certificates/placeholder.png";
+                          "https://community.softr.io/uploads/db9110/original/2X/7/74e6e7e382d0ff5d7773ca9a87e6f6f8817a68a6.jpeg";
                       }}
                     />
                   </div>
